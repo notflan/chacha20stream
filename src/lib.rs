9784208fc8e5a531766a1ebf1833f5278b273e61
Avril <flanchan@cumallover.me>
2021-08-13 16:22:29 +0100

    Added crate-level re-exports.
    Fortune for chacha20stream's current commit: Blessing − 吉

--- src/lib.rs
+++ src/lib.rs
@@ -63,8 +63,10 @@ mod bytes;
 
 #[cfg(feature="async")] mod stream_async;
 #[cfg(feature="async")] pub use stream_async::Sink as AsyncSink;
+#[cfg(feature="async")] pub use stream_async::Source as AsyncSource;
 
 pub use stream::Sink;
+pub use stream::Source;
 pub use key::{
     Key, IV,
 };
